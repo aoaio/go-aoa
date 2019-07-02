56c9538593fdff751ccca469f33ff67955ec829a
qiangkezhen <qiangkezhen@new4g.cn>
2019-07-02 11:32:28 +0800

    Add new node info

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -19,6 +19,10 @@ package params
 // MainnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
 // the main Aurora network.
 var MainnetBootnodes = []string{
+	"enode://6bf33bd5c9cbd6a560bc08b38be105dce8c0b25cabff93ae1012ad72c158c3baa56c4e6e330dcc27aa0985f9ed5b2a4a30e903f9583393aa3aa52602617eb89d@18.179.237.81:30303",
+	"enode://97fa7f37d9cf77bb12f2d44552763de6ffbbd123d92a7a5f5b59e6ac6606c5107acf894ad9bd645a98e7f34fe5c7dc3480db88ac65e02f0dac86ef261948df03@52.193.152.234:30303",
+	"enode://b8e5d467708c77459ab621d553e8d8720315ccc86b1dab0625542b8931edf5baa093a5dfcd87f27476609cfcc529d2fa8129ea83535ed2f5cf298cc19d328cae@18.182.210.180:30303",
+	"enode://0c94fdebb4be787700bc5aed3844bdd49ca156a6d1656fe85a84c8bee561272a4b92cad4a8dd89367cc092d9b6121e4ced8339df9cf1424815b6fbc3d91ed380@40.83.72.70:30303",
 }
 
 // TestnetBootnodes
