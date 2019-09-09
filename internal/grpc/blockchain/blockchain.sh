f69f638d3e044c8007de68f247dd075fa8bd53da
qiangkezhen <qiangkezhen@new4g.cn>
2019-09-09 13:14:45 +0800

    modify blockchain.sh file

---
+++ internal/grpc/blockchain/blockchain.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# add grpc path, may use proto file in this path
+grpc_path="/home/he/aoa/go/src/github.com/Aurorachain/go-aoa/internal/grpc/"
+file_name="blockchain"
+proto_dir="/home/he/aoa/go/src/github.com/Aurorachain/go-aoa/internal/grpc/${file_name}"
+#protoc --proto_path=${auraro_path} --proto_path=${personal_path} -I ${proto_dir} ${proto_dir}/${file_name}.proto --go_out=plugins=grpc:${proto_dir}
+protoc --proto_path=${grpc_path} --proto_path=${proto_dir} --go_out=plugins=grpc:${grpc_path} ${proto_dir}/${file_name}.proto
